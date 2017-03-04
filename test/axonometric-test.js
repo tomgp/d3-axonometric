ae7dff4ba540d6c16440d4f46698e59b34bdab7f
tomgp <tom.g.pearson@gmail.com>
2017-03-04 14:53:53 +0000

    add d3 path dependency

--- test/axonometric-test.js
+++ test/axonometric-test.js
@@ -1,5 +1,5 @@
 var tape = require("tape"),
-    foo = require("../");
+    axonometric = require("../");
 
 tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
   test.equal(axonometric.axonometric(), 42);
