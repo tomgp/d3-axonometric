8dedded959c25f30cc8045a0f61ba36652a03842
tomgp <tom.g.pearson@gmail.com>
2017-03-06 10:38:08 +0000

    Add path generator & helix example.

--- src/axonometric-line.js
+++ src/axonometric-line.js
@@ -1,3 +1,25 @@
+import {default as axonometricProjection}  from "./axonometric";
+
 export default function(){
-    return 'ax line';
+    var project = axonometricProjection();
+    //console.log(axonometricProjection);
+
+    function drawer (start, end){
+        var s = project(start);
+        var e = project(end);
+        return {
+            x1:s[0],
+            y1:s[1],
+            x2:e[0],
+            y2:e[1],
+        };
+    }
+
+    drawer.projection = function(p){
+        if(p !== undefined) return project; 
+        project = p;
+        return drawer;
+    }
+
+    return drawer;
 }
--- src/axonometric-path.js
+++ src/axonometric-path.js
@@ -1,3 +1,34 @@
+import {default as axonometricProjection}  from "./axonometric";
+import {default as d3Shape} from "d3-shape";
+
 export default function(){
-    return 'ax path';
+    var project = axonometricProjection();
+    var xAccessor = function(d){ return d[0]; };
+    var yAccessor = function(d){ return d[1]; };
+    var zAccessor = function(d){ return d[2]; };
+    var pathGenerator = d3Shape.line()
+        .x(function(d) { return d[0]; })
+        .y(function(d) { return d[1]; });
+
+    function drawer (data){
+        var pathData = data.map(function(d){
+            return project([xAccessor(d), yAccessor(d), zAccessor(d)]);
+        })
+
+        return pathGenerator(pathData);
+    }
+
+    drawer.projection = function(x){
+        if(x === undefined) return project; 
+        project = x;
+        return drawer;
+    }
+
+    drawer.pathGenerator = function(x){
+        if(x === undefined) return pathGenerator;
+        pathGenerator = x;
+        return drawer;
+    }
+
+    return drawer;
 }
